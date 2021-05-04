9b7b2bc457d948358aa3748ad7128ec9727aa69f
samratc <37380837+samratc@users.noreply.github.com>
2021-05-04 12:28:27 -0600

    practice commit

--- ReactWithBootstrap/src/lookups/countries.js
+++ ReactWithBootstrap/src/lookups/countries.js
@@ -1,6 +1,6 @@
-export const CountryList =[{name:'Select Country',value:''},
-{name:'India',value:'IN'},
-{name:'United States',value:'USA'},
-{name:'Australia',value:'AUS'},
-{name:'Newzeland',value:'NZ'}
+export const CountryList = [{ name: 'Select Country', value: '' },
+{ name: 'India', value: 'IN' },
+{ name: 'United States', value: 'USA' },
+{ name: 'Australia', value: 'AUS' },
+{ name: 'England', value: 'EL' }
 ];
